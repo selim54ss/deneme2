fca4b5206d4c59163ec4d7599317b9663931e824
selim54ss <seliminanir@gmail.com>
2022-12-21 18:17:40 +0300

    "static halledildi"

--- programlama_lab/pages/views.py
+++ programlama_lab/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("<h1> INDEX SAYAFASI</h1>")
+    return render(request, 'index.html')
+    
 # Create your views here.
